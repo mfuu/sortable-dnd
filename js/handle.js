b0ed7d2e0d9f19b186c25c8d95031756f15d3991
YOKE <mfuu1998@gmail.com>
2023-04-13 20:21:52 +0800

    update demo

--- js/handle.js
+++ js/handle.js
@@ -1,17 +1,17 @@
-const gridGroup = document.getElementById('gridGroup');
-const gridChildren = [];
+const handleGroup = document.getElementById('handleGroup');
+const handleChildren = [];
 for(let i = 0; i < 64; i++) {
   let li = document.createElement('li');
   li.innerHTML = `<i class="drag"></i>  <span>item ${i + 1}</span>`;
-  gridChildren.push(li);
+  handleChildren.push(li);
 }
-gridGroup.append(...gridChildren);
+handleGroup.append(...handleChildren);
 new Sortable(
-  gridGroup,
+  handleGroup,
   {
     animation: 150,
-    handle: '.drag',
     chosenClass: 'chosen',
+    handle: '.drag',
     onDrag: (e) => {
       console.log(e, 'ondrag')
     },
